29fbd0b069b4a17edcaa98671a507efde218ccb7
paulhornsey <paul@1partcarbon.co.uk>
2024-09-23 10:27:22 +0100

    Make Space-Space open fuzzy file find
    Also auto formated the file

--- lua/ninja/plugins/telescope.lua
+++ lua/ninja/plugins/telescope.lua
@@ -1,34 +1,35 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
 	dependencies = {
-    "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
 
-    telescope.setup({
-      defaults = {
-        path_display = { "smart" },
-        mappings = {
-          i = {
-            ["<C-k>"] = actions.move_selection_previous,
-            ["<C-j>"] = actions.move_selection_next,
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-          },
-        },
-      },
-    })
+		telescope.setup({
+			defaults = {
+				path_display = { "smart" },
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous,
+						["<C-j>"] = actions.move_selection_next,
+						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+					},
+				},
+			},
+		})
 
-    telescope.load_extension("fzf")
+		telescope.load_extension("fzf")
 
-    local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Fuzzy find files" })
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Fuzzy grep" })
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Fuzzy find in buffers" })
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Fuzzy help" })
-  end
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader><leader>", builtin.find_files, { desc = "Fuzzy find files" })
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Fuzzy find files" })
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Fuzzy grep" })
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Fuzzy find in buffers" })
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Fuzzy help" })
+	end,
 }
